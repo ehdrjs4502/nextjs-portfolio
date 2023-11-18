042e2d6e234990666b261ee47983b6a60c71293f
ehdrjs4502 <ehdrjs4502@naver.com>
2023-11-18 23:13:32 +0900

    프로젝트 타임라인 구현 중

--- src/sections/ProjectTimeLineSection.tsx
+++ src/sections/ProjectTimeLineSection.tsx
@@ -1,6 +1,37 @@
 import Title from "@/components/Title";
 
 export default function ProjectTimeLineSection() {
+    const projectList = [
+        {
+            date: "2023. 06 ~ 08 (3개월)",
+            name: "assemblog",
+            summaryList: [
+                <span>
+                    <strong>Next.js</strong>를 활용하여 개발한 2인 공동 블로그 개발 프로젝트
+                </span>,
+                <span>
+                    <strong>SEO</strong> 최적화를 위해 <strong>SSG</strong> 활용
+                </span>,
+                <span>
+                    <strong>TypeScript</strong>를 사용하여 컴파일 에러 방지
+                </span>,
+                <span>
+                    <strong>마크다운 형식</strong>의 글 작성 구현
+                </span>,
+                <span>
+                    <strong>ISR</strong>을 통해 블로그 게시글 업데이트
+                </span>,
+                <span>
+                    <strong>Next/Image</strong>를 활용하여 이미지 사이즈 최적화
+                </span>,
+            ],
+        },
+        {
+            date: "2023. 09 ~ 12 (4개월)",
+            name: "프로젝트2",
+            summaryList: ["로그인 구현", "로그아웃 구현"],
+        },
+    ];
     return (
         <>
             <div className="timeline-section">
@@ -10,27 +41,19 @@ export default function ProjectTimeLineSection() {
                         subTitle="가치 있는 개발자로 성장하기 위해 연도별로 진행한 프로젝트 목록입니다."
                     />
                     <div className="timeline-box">
-                        <div className="node">
-                            <div>2023. 03 ~ 06 (4개월)</div>
-                            <h3>Title</h3>
-                            <p>Status / Time</p>
-                        </div>
-                        <div className="node">
-                            <h3>Title</h3>
-                            <p>Status / Time</p>
-                        </div>
-                        <div className="node">
-                            <h3>Title</h3>
-                            <p>Status / Time</p>
-                        </div>
-                        <div className="node">
-                            <h3>Title</h3>
-                            <p>Status / Time</p>
-                        </div>
-                        <div className="node">
-                            <h3>Title</h3>
-                            <p>Status / Time</p>
-                        </div>
+                        {projectList.map((project) => (
+                            <div className="node">
+                                <div className="date">
+                                    <span>{project.date}</span>
+                                </div>
+                                <h2>{project.name}</h2>
+                                <ul>
+                                    {project.summaryList.map((text) => (
+                                        <li>{text}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
@@ -47,27 +70,46 @@ export default function ProjectTimeLineSection() {
 
                 .timeline-box {
                     margin-left: 1.5rem;
-                    border-left: 3px solid rgb(255, 175, 175);
+                    border-left: 3px solid #5ba6ff;
                 }
+
                 .node {
-                    padding-left: 0.5rem;
+                    padding-left: 1.5rem;
                     padding-bottom: 1.5rem;
                     position: relative;
                 }
-                .node h3,
+
+                .node h2 {
+                    color: #5ba6ff;
+                }
+                .node h2,
                 .node p {
                     margin: 0;
                 }
+
                 .node::before {
                     content: "";
                     width: 0.5rem;
                     height: 0.5rem;
-                    background: #fff;
-                    border: 2px solid rgb(255, 175, 175);
+                    background: #5ba6ff;
+                    border: 2px solid #5ba6ff;
                     border-radius: 50%;
                     position: absolute;
-                    top: 0.3rem;
+                    top: 0.9em;
                     left: -0.5rem;
+                }
+
+                .date {
+                    display: inline-block;
+                    padding: 10px;
+                    border-radius: 12px;
+                    background: #d7ecff;
+                    margin-bottom: 12px;
+                }
+
+                .date span {
+                    color: #5ba6ff;
+                    font-weight: 600;
                 }
             `}</style>
         </>
