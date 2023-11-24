683dc0926d70544011d74200c55c4236e700bf7f
ehdrjs4502 <ehdrjs4502@naver.com>
2023-11-25 00:36:30 +0900

    프로젝티 상세 섹션 구현 중

--- src/app/_sections/ProjectDetailSection.tsx
+++ src/app/_sections/ProjectDetailSection.tsx
@@ -1,9 +1,20 @@
 import Title from "../_components/Title";
+import styles from "@/app/_styles/sections/ProjectDetailSection.module.css";
 
 export default function ProjectDetailSection() {
   return (
-    <div>
-      <Title title="프로젝트 소개" subTitle="진행한 프로젝트 상세 설명입니다." />
-    </div>
+    <section className={styles.section}>
+      <div>
+        <Title title="프로젝트 소개" subTitle="진행한 프로젝트 상세 설명입니다." />
+        <div className={styles.box}>
+          <div>
+            <span>프로젝트 화면(슬라이더)</span>
+          </div>
+          <div>
+            <h4>프로젝트 제목</h4>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
