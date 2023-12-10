efa9ac389d2f082f7d6262b3faf53e4ddba08b5c
ehdrjs4502 <ehdrjs4502@naver.com>
2023-12-10 18:39:12 +0900

    애니메이션 구현

--- src/app/_components/SkillBox.tsx
+++ src/app/_components/SkillBox.tsx
@@ -1,6 +1,9 @@
+"use client";
 import styles from "@/app/_styles/components/SkillBox.module.css";
 import { Tooltip } from "@mui/material";
 import { ReactNode } from "react";
+import Reveal from "react-awesome-reveal";
+import { keyframes } from "@emotion/react";
 
 type Skill = {
   name: string;
@@ -17,16 +20,30 @@ type SkillBoxProps = {
   skills: (Skill | Etc)[];
 };
 
+const customAnimation: any = keyframes`
+  from {
+    opacity: 0;
+    transform: translate3d(0, 20%, 0);
+  }
+
+  to {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+`;
+
 export default function SkillBox({ skills }: SkillBoxProps) {
   return (
     <div className={styles.box}>
-      {skills.map((data: any, index: number) => (
-        <div key={index} className={styles.icon}>
-          <Tooltip title={data.name} arrow placement="top">
-            {data.icon}
-          </Tooltip>
-        </div>
-      ))}
+      <Reveal keyframes={customAnimation} cascade damping={0.2}>
+        {skills.map((data: any, index: number) => (
+          <div key={index} className={styles.icon}>
+            <Tooltip title={data.name} arrow placement="top">
+              {data.icon}
+            </Tooltip>
+          </div>
+        ))}
+      </Reveal>
     </div>
   );
 }
--- src/app/_sections/ProjectTimeLineSection.tsx
+++ src/app/_sections/ProjectTimeLineSection.tsx
@@ -1,6 +1,21 @@
+"use client";
 import Title from "@/app/_components/Title";
 import styles from "@/app/_styles/sections/ProjectTimeLineSection.module.css";
 import { projectTimeLineData } from "../_data/projectTimeLineData";
+import Reveal from "react-awesome-reveal";
+import { keyframes } from "@emotion/react";
+
+const customAnimation: any = keyframes`
+  from {
+    opacity: 0;
+    transform: translate3d(0, 10%, 0);
+  }
+
+  to {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+`;
 
 export default function ProjectTimeLineSection() {
   return (
@@ -12,20 +27,22 @@ export default function ProjectTimeLineSection() {
             subTitle="가치 있는 개발자로 성장하기 위해 연도별로 진행한 프로젝트 목록입니다."
           />
           <div className={styles.box}>
-            {projectTimeLineData.map((project) => (
-              <div className={styles.node}>
-                <div className={styles.date}>
-                  <span>{project.date}</span>
+            <Reveal keyframes={customAnimation} cascade damping={0.5}>
+              {projectTimeLineData.map((project) => (
+                <div className={styles.node}>
+                  <div className={styles.date}>
+                    <span>{project.date}</span>
+                  </div>
+                  <h2>{project.name}</h2>
+                  <span className={styles.type}>{project.projectType}</span>
+                  <ul>
+                    {project.summaryList.map((text) => (
+                      <li className={styles.summary}>{text}</li>
+                    ))}
+                  </ul>
                 </div>
-                <h2>{project.name}</h2>
-                <span className={styles.type}>{project.projectType}</span>
-                <ul>
-                  {project.summaryList.map((text) => (
-                    <li className={styles.summary}>{text}</li>
-                  ))}
-                </ul>
-              </div>
-            ))}
+              ))}
+            </Reveal>
           </div>
         </div>
       </section>
