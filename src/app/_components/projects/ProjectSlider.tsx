793f458d5a9cd383aafa5d10579a8104a911eb63
ehdrjs4502 <ehdrjs4502@naver.com>
2023-12-29 20:50:49 +0900

    프로젝트 슬라이더 다크 모드 스타일 수정

--- src/app/_components/projects/ProjectSlider.tsx
+++ src/app/_components/projects/ProjectSlider.tsx
@@ -4,23 +4,40 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import ProjectDetail from "./ProjectDetail";
 import { projectDetailsData } from "../../_data/projectDetailsData"; // 데이터 불러오기
+import { useCallback, useRef } from "react";
+import styles from "@/app/_styles/components/ProjectSlider.module.css";
+import { ArrowForwardIos, ArrowBackIosNew } from "@mui/icons-material";
 
 export default function ProjectSlider() {
+  const slickRef = useRef<Slider>(null); // 슬라이더에 쓸거니 <Slider> 타입
+
+  const previous = useCallback(() => slickRef.current?.slickPrev(), []);
+  const next = useCallback(() => slickRef.current?.slickNext(), []);
+
   const settings = {
     dots: true, // 캐러셀 밑에 ... 을 표시할지
     infinite: true, // 슬라이드가 끝까지 가면 다시 처음으로 반복
     speed: 500, // 속도
     slidesToShow: 1, // 한 번에 보여줄 슬라이드 개수
     slidesToScroll: 1, // 한 번에 넘어가는 슬라이드 개수
+    arrows: false,
   };
 
   return (
     <div>
-      <Slider {...settings}>
+      <Slider {...settings} ref={slickRef}>
         {projectDetailsData.map((project, idx: number) => (
           <ProjectDetail key={idx} project={project} />
         ))}
       </Slider>
+      <div className={styles.arrowBox}>
+        <div onClick={previous}>
+          <ArrowBackIosNew />
+        </div>
+        <div onClick={next}>
+          <ArrowForwardIos />
+        </div>
+      </div>
     </div>
   );
 }
