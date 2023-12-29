d18b5e901d6a6c51f26d6450e451acb86f9d04b1
ehdrjs4502 <ehdrjs4502@naver.com>
2023-12-29 19:44:47 +0900

    다크 테마 수정

--- src/app/_data/skillData.tsx
+++ src/app/_data/skillData.tsx
@@ -1,8 +1,9 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import HtmlIcon from "../../../public/svgs/html5-color.svg";
 import CssIcon from "../../../public/svgs/css3-color.svg";
 import JsIcon from "../../../public/svgs/javascript-color.svg";
 import NextjsIcon from "../../../public/svgs/nextdotjs-color.svg";
+import NextjsDarkIcon from "../../../public/svgs/nextdotjs-dark.svg";
 import ReactjsIcon from "../../../public/svgs/react-color.svg";
 import NodejsIcon from "../../../public/svgs/nodedotjs-color.svg";
 import NotionIcon from "../../../public/svgs/notion-color.svg";
@@ -33,7 +34,17 @@ type DarkModeIconProps = {
 
 // 테마에 따라서 아이콘 변경
 const DarkModeIcon: React.FC<DarkModeIconProps> = ({ lightIcon, darkIcon }) => {
+  const [mounted, setMounted] = useState(false);
   const { theme } = useTheme();
+
+  // ssr에서 말고 클라이언트 측에서 렌더링 되고 나서 아이콘을 렌더링하도록 설정
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  console.log("Current Theme:", theme);
 
   return theme === "dark" ? darkIcon : lightIcon;
 };
