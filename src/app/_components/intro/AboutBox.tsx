408b7cff27cc821cf65e96f72be1220a82e4bc2d
ehdrjs4502 <ehdrjs4502@naver.com>
2024-02-22 21:06:43 +0900

    about me 내용 변경

--- src/app/_components/intro/AboutBox.tsx
+++ src/app/_components/intro/AboutBox.tsx
@@ -29,12 +29,12 @@ export default function AboutBox() {
     <motion.div className={styles.aboutBox} variants={list} initial="hidden" animate="visible">
       <h5>About Me</h5>
       <motion.span variants={item}>
-        <strong className={styles.strongBlue}>React.js</strong>, <strong style={{ color: "#3C3C3C" }}>Next.js</strong>를
-        활용하여 <strong>프론트엔드 개발</strong>을 수행하는 개발자
+        항상 <strong className={styles.strongBlue}>긍정적인 사고</strong>로 어떤 어려움도{" "}
+        <strong>포기하지 않고 프로젝트를 진행</strong>하는 개발자입니다.
       </motion.span>
       <motion.span variants={item}>
-        팀과 개인을 가리지 않고, 여러 프로젝트 경험으로 <strong style={{ color: "#5EA8FF" }}>실력</strong>과{" "}
-        <strong style={{ color: "#FF4F4F" }}>실무 감각</strong>을 쌓아가는 개발자
+        꾸준히 <strong>코딩테스트 문제</strong>를 해결하여 <strong className={styles.strongOrange}>코딩 실력</strong>과{" "}
+        <strong className={styles.strongBlue}>문제 해결 능력</strong>을 기르고 있는 개발자입니다.
       </motion.span>
     </motion.div>
   );
